77d1d3a46b1292e64d91b900aeb6dde347f8fcf9
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-02 21:42:04 -0400

    Added sample projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
+// TODO: Include a short description of the project
+// TODO: Include used languages and tools
+// TODO: Add gif as sample images
+// TODO: Improve button effects
+// TODO: Find better color palete
+
 class Project extends Component {
   render() {
     return (
       <div id = "project-section">
         <h2>Projects</h2>
+
+        <div className = "project-container">
+          <div className = "project-box">
+            <h4>DST Lookup Tool</h4>
+            <div className = "project-img"></div>
+            <a href = "" target = "_bank">Demo</a>
+            <a href = "" target = "_blank">Code</a>
+          </div>
+          <div className = "project-box">
+            <h4>Dillopass</h4>
+            <div className = "project-img"></div>
+            <a href = "" target = "_bank">Demo</a>
+            <a href = "" target = "_blank">Code</a>
+          </div>
+          <div className = "project-box">
+            <h4>Greenpaws</h4>
+            <div className = "project-img"></div>
+            <a href = "" target = "_bank">Demo</a>
+            <a href = "" target = "_blank">Code</a>
+          </div>
+          <div className = "project-box">
+            <h4>Untitled</h4>
+            <div className = "project-img"></div>
+            <a href = "" target = "_bank">Demo</a>
+            <a href = "" target = "_blank">Code</a>
+          </div>
+        </div>
       </div>
     )
   }

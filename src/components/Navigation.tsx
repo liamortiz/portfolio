d37bcd1d16dc99c68827b92b5dec1a28ba6eebb2
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-09 19:09:27 -0400

    Fixed nav bug

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation : React.FC = () => {
 
   function scroll(element : any) : void {
-    element.scrollIntoView({behavior : 'smooth', block : 'start'});
+    element.scrollIntoView({behavior : 'smooth', block : 'end'});
   }
 
   return (
@@ -12,7 +12,7 @@ const Navigation : React.FC = () => {
         <li><div className = "active" onClick = {() => scroll(document.getElementById('main-header'))}>Home</div><span></span></li>
         <li><div onClick = {() => scroll(document.getElementById('project-section'))}>Projects</div><span></span></li>
         <li><div onClick = {() => scroll(document.getElementById('skills-section'))}>Skills</div><span></span></li>
-        <li><div onClick = {() => scroll(document.getElementById('main-header'))}>About</div><span></span></li>
+        <li><div onClick = {() => scroll(document.getElementById('about-section'))}>About</div><span></span></li>
         <li><div onClick = {() => scroll(document.getElementById('main-header'))}>Contact</div><span></span></li>
       </ul>
     </nav>

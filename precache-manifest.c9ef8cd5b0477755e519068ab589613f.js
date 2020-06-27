56de065bec6acadbb01a12ef80e831639912f588
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-26 20:27:58 -0400

    Updates

---
+++ precache-manifest.c9ef8cd5b0477755e519068ab589613f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ad9fc3907ceb6f336abc3a49fb8f8b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6a07dd31dc74f598540",
+    "url": "/portfolio/static/css/main.ba6645e5.chunk.css"
+  },
+  {
+    "revision": "cefbfe9636a5ee28a9a6",
+    "url": "/portfolio/static/js/2.bc221be9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.bc221be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a07dd31dc74f598540",
+    "url": "/portfolio/static/js/main.c207d8e3.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/portfolio/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "cc347a05e242c636fc9c84c930890d83",
+    "url": "/portfolio/static/media/arrow.cc347a05.svg"
+  },
+  {
+    "revision": "2c90e9e7aa651a2ab8f93cbeb191383f",
+    "url": "/portfolio/static/media/dstpreview.2c90e9e7.png"
+  },
+  {
+    "revision": "a53c5d7720f5d4b735dd63dff65aaacd",
+    "url": "/portfolio/static/media/git.a53c5d77.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/portfolio/static/media/github-logo.192a6620.svg"
+  },
+  {
+    "revision": "3ac59aa5df5d2fe2c9de0bd3b5bafec2",
+    "url": "/portfolio/static/media/hero.3ac59aa5.jpg"
+  },
+  {
+    "revision": "9b574ac5ad8b7a23d74df0eb5227c9b4",
+    "url": "/portfolio/static/media/html5_icon.9b574ac5.svg"
+  },
+  {
+    "revision": "684127fbaf4a8253ddc89a45d9c6911d",
+    "url": "/portfolio/static/media/js_icon.684127fb.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "44a823e30de9efe22f3fa8243f555c04",
+    "url": "/portfolio/static/media/portfolio.44a823e3.PNG"
+  },
+  {
+    "revision": "ab4b1a319eb6c85373b43deea500499f",
+    "url": "/portfolio/static/media/programmer.ab4b1a31.svg"
+  },
+  {
+    "revision": "f929fcc3f6c9e698c4af0f805c04f72c",
+    "url": "/portfolio/static/media/python_icon.f929fcc3.svg"
+  },
+  {
+    "revision": "bcb113b9659ec9a26133a0efdd38aff0",
+    "url": "/portfolio/static/media/rails_icon.bcb113b9.svg"
+  },
+  {
+    "revision": "5f0931d4e6a64cde862793480acd17cd",
+    "url": "/portfolio/static/media/react_icon.5f0931d4.svg"
+  },
+  {
+    "revision": "84d18de44fef8d070b672cb0be4529f8",
+    "url": "/portfolio/static/media/ruby_icon.84d18de4.svg"
+  },
+  {
+    "revision": "4e3c2a4c4eaa023673cb82419f2a1134",
+    "url": "/portfolio/static/media/sass_icon.4e3c2a4c.svg"
+  },
+  {
+    "revision": "21cf297fe9a84c3bd92587ad507a6f00",
+    "url": "/portfolio/static/media/spotify_demo.21cf297f.png"
+  },
+  {
+    "revision": "8fbc017ccf26a288d9d79ea40f49b667",
+    "url": "/portfolio/static/media/sql.8fbc017c.svg"
+  },
+  {
+    "revision": "2dcf8108c2fae5ebebea75e4358ca9f3",
+    "url": "/portfolio/static/media/typescript.2dcf8108.svg"
+  }
+]);

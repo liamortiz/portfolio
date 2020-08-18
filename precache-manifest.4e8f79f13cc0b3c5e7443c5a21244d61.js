d72b1c023e2d6d046df163c47653de0a141da448
Leamsi Escribano <leamsi.esc@gmail.com>
2020-08-17 20:53:12 -0400

    Updates

---
+++ precache-manifest.4e8f79f13cc0b3c5e7443c5a21244d61.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5326565fa5cbd27a4207f38e72c035e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07de6eac57f4b0b0b445",
+    "url": "/static/css/main.ccfa3ee8.chunk.css"
+  },
+  {
+    "revision": "96111151ea7b14846474",
+    "url": "/static/js/2.389470d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.389470d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de6eac57f4b0b0b445",
+    "url": "/static/js/main.f7b8f225.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "6f85afcd6c3b003221645e2ee1d0442f",
+    "url": "/static/media/algi.6f85afcd.gif"
+  },
+  {
+    "revision": "21bb4310aa4ae40032bc4b21f43f7354",
+    "url": "/static/media/book_corner.21bb4310.PNG"
+  },
+  {
+    "revision": "055d90f9d4d4df574f76bd65222a3edf",
+    "url": "/static/media/dstpreview.055d90f9.gif"
+  },
+  {
+    "revision": "a53c5d7720f5d4b735dd63dff65aaacd",
+    "url": "/static/media/git.a53c5d77.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/static/media/github-logo.192a6620.svg"
+  },
+  {
+    "revision": "468c7f3327aee449c1629bc5224c051c",
+    "url": "/static/media/hero.468c7f33.jpg"
+  },
+  {
+    "revision": "9b574ac5ad8b7a23d74df0eb5227c9b4",
+    "url": "/static/media/html5_icon.9b574ac5.svg"
+  },
+  {
+    "revision": "684127fbaf4a8253ddc89a45d9c6911d",
+    "url": "/static/media/js_icon.684127fb.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "8d31b8d76aa497dc92da748fdd43290e",
+    "url": "/static/media/nav.8d31b8d7.svg"
+  },
+  {
+    "revision": "44a823e30de9efe22f3fa8243f555c04",
+    "url": "/static/media/portfolio.44a823e3.PNG"
+  },
+  {
+    "revision": "ab4b1a319eb6c85373b43deea500499f",
+    "url": "/static/media/programmer.ab4b1a31.svg"
+  },
+  {
+    "revision": "f929fcc3f6c9e698c4af0f805c04f72c",
+    "url": "/static/media/python_icon.f929fcc3.svg"
+  },
+  {
+    "revision": "bcb113b9659ec9a26133a0efdd38aff0",
+    "url": "/static/media/rails_icon.bcb113b9.svg"
+  },
+  {
+    "revision": "5f0931d4e6a64cde862793480acd17cd",
+    "url": "/static/media/react_icon.5f0931d4.svg"
+  },
+  {
+    "revision": "84d18de44fef8d070b672cb0be4529f8",
+    "url": "/static/media/ruby_icon.84d18de4.svg"
+  },
+  {
+    "revision": "4e3c2a4c4eaa023673cb82419f2a1134",
+    "url": "/static/media/sass_icon.4e3c2a4c.svg"
+  },
+  {
+    "revision": "21cf297fe9a84c3bd92587ad507a6f00",
+    "url": "/static/media/spotify_demo.21cf297f.png"
+  },
+  {
+    "revision": "8fbc017ccf26a288d9d79ea40f49b667",
+    "url": "/static/media/sql.8fbc017c.svg"
+  },
+  {
+    "revision": "2dcf8108c2fae5ebebea75e4358ca9f3",
+    "url": "/static/media/typescript.2dcf8108.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c4c7328eded63a12ffd3eefba5f3d6da.js"
+  "/precache-manifest.4e8f79f13cc0b3c5e7443c5a21244d61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
